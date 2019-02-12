8ab7d09fc8cc49a020d7f50ad2c1c35d6d271615
jenifercoila <jeniferfcr@gmail.com>
2019-02-11 20:21:05 -0500

    Agregando test de mis funciones

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -20,6 +20,7 @@ export const viewTemplates = {
                 <button type="button" id="btn-facebook" class="btn-social-net">
                 <button type="button" id="btn-google" class="btn-social-net">
                 </div>`;
+
     const element = document.createElement('form');
     element.innerHTML = tmpl;
 
@@ -45,17 +46,17 @@ export const viewTemplates = {
   },
 
   signUp: () => {
-    const tmpl = `<div class="login-form" id="sign-up">
-                <input type="text" id="enter-name" class="enter-data" placeholder="Nombres" required> 
-                <input type="text" id="enter-lastname" class="enter-data" placeholder="Apellidos" required> 
-                <input type="text" id="enter-nick" class="enter-data" placeholder="Alias" required> 
-                <input type="text" id="enter-country" class="enter-data" placeholder="País" required>
-                <input type="email" id="enter-email" class="enter-data" placeholder="Correo electrónico" required> <span id="verif-email"></span>
-                <input type="password" id="enter-psw" class="enter-data" placeholder="Contraseña" required minlength="8"> 
-                <input type="password" id="re-enter-psw" class="enter-data" placeholder="Vuelve a ingresar tu contraseña" required minlength="8"> <span id="verif-pass"></span>
-                <button type="button" id="send-data-btn" class="button-send"> Registrarme</button>
-                <p id="error-text-sign-up"></p>
-                </div>`;
+    const tmpl = `<div class='login-form' id='sign-up'>
+                  <input type='text' id='enter-name' class='enter-data' placeholder='Nombres' required> 
+                  <input type='text' id='enter-lastname' class='enter-data' placeholder='Apellidos' required> 
+                  <input type='text' id='enter-nick' class='enter-data' placeholder='Alias' required> 
+                  <input type='text' id='enter-country' class='enter-data' placeholder='País' required>
+                  <input type='email' id='enter-email' class='enter-data' placeholder='Correo electrónico' required> <span id='verif-email'></span>
+                  <input type='password' id='enter-psw' class='enter-data' placeholder='Contraseña' required minlength='8'> 
+                  <input type='password' id='re-enter-psw' class='enter-data' placeholder='Vuelve a ingresar tu contraseña' required minlength='8'> <span id='verif-pass'></span>
+                  <button type='button' id='send-data-btn' class='button-send'> Registrarme</button>
+                  <p id='error-text-sign-up'></p>
+                  </div>`;
     const element2 = document.createElement('form');
     element2.setAttribute('class', 'form-signUp');
     element2.innerHTML = tmpl;
@@ -68,19 +69,36 @@ export const viewTemplates = {
   },
 
   home: (posts) => {
-    const tmpl = `<div class="log-out-form" id="log-out">
-                  <h1> Bienvenido</h1>
-                  <button id="log-out-btn" class="button-send">Salir</button>
+    const tmpl = `<header class='header-page'>
+                  <div><img class='img-logo  align top top' src='img/logo.png' alt='logo-feminista'>
+                  <input class='menu-bar' type='checkbox' id='menu-bar'>
+                  <label class='icon-menu' for='menu-bar'><img src='img/boton-menu.png' alt='icono de menu' class='img-menu  align top'></label>
+                  <nav class='menu-nav'>
+                    <ul class='menu-ul'>
+                      <li class='li-menu'><a class='profile'><img class='img-logo-2 align align top' src='img/usuario-3.png' alt='icono'><h2 class='text-4 margin'>Perfil</h2></a></li>
+                      <li class='li-menu2'><a class='items'><img class='img-logo  align top' src='img/historia.png' alt='icono'><h2 class='text-3'>Historias</h2></a></li>
+                      <li class='li-menu2'><a class='items'><img class='img-logo  align top' src='img/calendario.png' alt='icono'><h2 class='text-3'>Eventos</h2></a></li>
+                      <li class='li-menu2'><a class='items'><img class='img-logo  align top' src='img/grupo.png' alt='icono'><h2 class='text-3'>Grupos</h2></a></li>
+                      <li class='li-menu2'><a class='items'><img class='img-logo  align top' src='img/mundo.png' alt='icono'><h2 class='text-3'>Comunidades</h2></a></li>
+                      <li class='li-menu2'><a class='items'><img class='img-logo  align top' src='img/ley.png' alt='icono'><h2 class='text-3'>Apoyo Legal</h2></a></li>
+                      <li class='li-menu2'><a class='items'><img class='img-logo  align top' src='img/apoyar-3.png' alt='icono'><h2 class='text-3'>Apoyo Psicologico</h2></a></li>
+                      <li class='li-menu2'><a class='items' id='log-out-btn'><img class='img-logo  align top' src='img/salir.png' alt='icono'><h2 class='text-3'>Salir</h2></a></li>
+                    </ul>
+                  </nav><h1 class='text-logo  align top'>JoinClude</h1></div>
+                  </header>
+                  <div class='log-out-form' id='log-out'>
+                  <h1 class='text-3'>Bienvenido</h1>
                   </div>
-                  <div>
-                  <textarea class="box-post" name="post-input" id="post-input" cols="50" rows="10"></textarea>
-                  <select id='privacy-selector'>
-                    <option value="Público">Publico</option>
-                    <option value="Amigos">Amigos</option>
+                  <div class='box-post large'>
+                  <textarea class='box-message' name='post-input' id='post-input' cols='50' rows='10'></textarea>
+                  <span id='post-error'></span>
+                  <select id='privacy-selector' class='select-privacy'>
+                    <option value='Público'>Publico</option>
+                    <option value='Privado'>Privado</option>
                   </select>
-                  <button id="btn-posts" class="btn-post">Publicar</button>
+                  <button id='btn-posts' class='btn-post'>Publicar</button>
                 </div>
-                <ul id= "post-container"> </ul>
+                <ul id='post-container' class='list-posts'></ul>
                 `;
     const section = document.createElement('section');
     section.innerHTML = tmpl;
@@ -98,7 +116,6 @@ export const viewTemplates = {
       posts.forEach(post => {
         /* Aplico un forEach para añadir cada post  a mi ul aplicando la
          funcion de templates notefunction */
-        console.log(post);
         postContainer.appendChild(noteFunction(post));
       });
     });
@@ -121,16 +138,23 @@ export const viewTemplates = {
 
 /* Funcion con el maquetado de mis post - JENI*/
 
-const noteFunction = (post) => {
-  const tmp = `
-  <p> ${post.name} dice </p>
-    <span> ${post.content} </span>
-    <span> ${post.privacy} </span>
-    <button class="" id="btn-edit-${post.id}">
-      Editar </button>
-    <button class="" id="btn-deleted-${post.id}">
-      Eliminar </button>`;
+export const noteFunction = (post) => {
+  const tmp = `<div class='box-post large2'>
+      <div class='box-post-message'>
+      <img src='img/usuario-3.png' alt='icono de perfil' class='img-menu align-2'>
+      <h2 class='text-5'>${post.name} dice
+      <div class='icon-privacy'>Hace un momento ${post.privacy}</div></h2>
+      <p class='content-edit'>${post.content}</p>
+      </div>
+      <div class='count-like'><img src='img/corazon.png' alt='icono de like' class='img-like align'><h2 class='like-word'>Like</h2></div>
+      <button class='btn-post btn-edit-delete' id='btn-edit-${post.id}'>Editar</button>
+      <button class='btn-post btn-edit-delete' id='btn-deleted-${post.id}'>Eliminar</button></div>`;
   let postList = document.createElement('div');
+  postList.setAttribute('id', `id-${post.id}`);
   postList.innerHTML = tmp;
+
+  const btnDeletePost = postList.querySelector(`#btn-deleted-${post.id}`);
+  btnDeletePost.addEventListener('click', () => deletePost(post.id));
+
   return postList;
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,66 @@
-// importamos la funcion que vamos a testear
-import { myFunction } from "../src/lib/index";
+// configurando firebase mock
+const firebasemock = require('firebase-mock');
+const mockauth = new firebasemock.MockFirebase();
+const mockfirestore = new firebasemock.MockFirestore();
+mockfirestore.autoFlush();
+mockauth.autoFlush();
 
-describe('myFunction', () => {
+global.firebase = firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  path => (path ? mockdatabase.child(path) : null),
+  () => mockauth,
+  () => mockfirestore
+);
+
+// importamos la funcion que vamos a testear
+import { signInUser, loginAuth, closeSignIn, signUpUser } from '../src/lib/firebase/controller-firebase.js';
+
+describe('signInUser', () => {
   it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+    expect(typeof signInUser).toBe('function');
+  });
+  it('Debería poder iniciar sesion', () => {
+    return signInUser('front@end.la', '123456')
+      .then((user) => {
+        expect(user.email).toBe('front@end.la');
+      });
   });
 });
+
+describe('loginAuth', () => {
+  it('debería ser una función', () => {
+    expect(typeof loginAuth).toBe('function');
+  });
+  it('Debería poder auntentificar el email y contraseña', () => {
+    return loginAuth(() => {
+      
+    });
+  });
+});
+
+describe('closeSignIn', () => {
+  it('debería ser una función', () => {
+    expect(typeof closeSignIn).toBe('function');
+  });
+  it('Debería poder salir de sesion de la cuenta de la red social', () => {
+    return closeSignIn();
+  });
+});
+
+describe('signUpUser', () => {
+  it('debería ser una función', () => {
+    expect(typeof signUpUser).toBe('function');
+  });
+  it('Debería poder registrar a un usuario', () => {
+    return signUpUser('margarita12@gmail.com', '234567');
+  });
+});
+
+// describe('D', () => {
+//   it('debería ser una función', () => {
+//     expect(typeof signUpUser).toBe('function');
+//   });
+//   it('Debería poder registrar a un usuario', () => {
+//     return signUpUser('front@end.la', '123456');
+//   });
+// });
